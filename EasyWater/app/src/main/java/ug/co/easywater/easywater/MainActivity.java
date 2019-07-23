15fbb155b20f66296b37296ef23ac43c98566c9f
simonojok19 <simonojok19@gmail.com>
2019-07-23 09:28:30 +0300

    added action bar

--- EasyWater/app/src/main/java/ug/co/easywater/easywater/MainActivity.java
+++ EasyWater/app/src/main/java/ug/co/easywater/easywater/MainActivity.java
@@ -3,6 +3,8 @@ package ug.co.easywater.easywater;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actions, menu);
+        return(super.onCreateOptionsMenu(menu));
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.gone:
+                // do soomething
+                return (true);
+            case R.id.help:
+                // do something
+                return (true);
+            case R.id.history:
+                // do something
+                return (true);
+            case R.id.pay:
+                // do something
+                return (true);
+            case R.id.usage:
+                // do something
+                return (true);
+        }
+
+
+
+        return (super.onOptionsItemSelected(item));
+
     }
 }
