7360edba8a542ab71dde1d8294f6cb8829b20881
simonojok19 <simonojok19@gmail.com>
2019-07-23 07:59:13 +0300

    rechecking

--- backend/customer/models.py
+++ backend/customer/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+"""
+    Django already provide us with a model which stores information about
+    the user like Username, First Name, LastName,
+    Email Address
+    We shall Use those information here
+"""
+User = get_user_model()
+
+class Profile(models.Model):
+    ccupation       = models.CharField(max_length=20)
+    nationality     = models.CharField(max_length=20)
+    work_place      = models.CharField(max_length=20)
+
+
+class Identification(models.Model):
+    id_type   = models.CharField(max_length=20)
+    id_number = models.CharField(max_length=30)
+    address   = models.TextField()
+
