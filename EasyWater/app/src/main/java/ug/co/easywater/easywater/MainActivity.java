bc8ab7e6170fb6626ee4c1624f11ac71674eb968
simonojok19 <simonojok19@gmail.com>
2019-07-23 08:58:27 +0300

    Created the application structure, waiting for lawrance to add decorder in java codes

---
+++ EasyWater/app/src/main/java/ug/co/easywater/easywater/MainActivity.java
@@ -0,0 +1,14 @@
+package ug.co.easywater.easywater;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
