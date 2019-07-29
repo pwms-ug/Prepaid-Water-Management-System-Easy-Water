39c47c545243aebb429fdb1e828876930746cf1a
simonojok19 <simonojok19@gmail.com>
2019-07-29 08:48:01 +0300

    worked on a single webpage

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('safewater.urls')), # library is the starting point of the application
-    path('user/', include('user.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
